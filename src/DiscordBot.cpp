3116e15fd0638a98bf1459d451272a709fa22180
Burfedd <leonquad2@gmail.com>
2022-03-22 13:19:10 +0200

    Added token parse from a text file

--- src/DiscordBot.cpp
+++ src/DiscordBot.cpp
@@ -1,8 +1,20 @@
 ﻿#include "DiscordBot.h"
 #include <iostream>
+#include <dpp/dpp.h>
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
+	std::string botToken;
+	std::ifstream file("../token.txt");
+	if (file.is_open()) {
+		std::getline(file, botToken);
+	}
+	file.close();
+	if (botToken == "") {
+		std::cout << "You didn't specify the bot token! Aborting..." << std::endl;
+		return 0;
+	}
+
+	std::cout << "Bot token: " << botToken << std::endl;
 	return 0;
 }
