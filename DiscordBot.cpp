3a3bdd2fcf474ac14b1118756388dc0772084638
Burfedd <leonquad2@gmail.com>
2022-03-22 10:43:52 +0200

    Add project files.

---
+++ DiscordBot.cpp
@@ -0,0 +1,12 @@
+﻿// DiscordBot.cpp : Defines the entry point for the application.
+//
+
+#include "DiscordBot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
